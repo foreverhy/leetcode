6e2f7299da45d0e88872590bf9efc643f09ac0da
Yao Han <verihy@gmail.com>
2015-04-01 22:18:26 +0800

    leet.h

--- leet.cc
+++ leet.cc
@@ -1,42 +1,5 @@
-#include <vector>
-#include <string>
-#include <iostream>
+#include "leet.h"
 
-using std::vector;
-using std::string;
-using std::cin;
-using std::cout;
-using std::endl;
-
-
-#include <cstdlib>
-using std::atoi;
-struct ListNode{
-    int val;
-    ListNode *next;
-    ListNode(int x):val(x), next(nullptr){}
-    
-    ListNode(std::initializer_list<int> vals):val(0), next(nullptr){
-        if(0 == vals.size()){
-            return;
-        }
-        auto p = vals.begin();
-        this->val = *p;
-        auto last = this;
-        for(++p; p != vals.end(); ++p){
-            last->next = new ListNode(*p);
-            last = last->next;
-        }
-    }
-
-};
-
-std::ostream& operator<<(std::ostream& os, ListNode *head){
-    for(; head; head = head->next){
-        os << head->val << " ";
-    }
-    return os;
-}
 
 
 
---
+++ leet.h
@@ -0,0 +1,56 @@
+#ifndef LEET_H
+#define LEET_H
+#include <vector>
+#include <string>
+#include <iostream>
+
+using std::vector;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+#include <cstdlib>
+using std::atoi;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr){}
+};
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x), next(nullptr){}
+    
+    ListNode(std::initializer_list<int> vals):val(0), next(nullptr){
+        if(0 == vals.size()){
+            return;
+        }
+        auto p = vals.begin();
+        this->val = *p;
+        auto last = this;
+        for(++p; p != vals.end(); ++p){
+            last->next = new ListNode(*p);
+            last = last->next;
+        }
+    }
+
+};
+
+std::ostream& operator<<(std::ostream& os, ListNode *head){
+    for(; head; head = head->next){
+        os << head->val << " ";
+    }
+    return os;
+}
+
+
+
+
+
+
+#endif
