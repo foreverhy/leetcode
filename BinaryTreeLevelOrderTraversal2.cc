cb72f41671db732e191807baf9eadbc931106d55
Yao Han <verihy@gmail.com>
2015-04-28 19:47:51 +0800

    Update

---
+++ BinaryTreeLevelOrderTraversal2.cc
@@ -0,0 +1,71 @@
+#include "leet.h"
+
+
+
+
+#include <algorithm>
+#include <cstring>
+
+#include <queue>
+
+class Solution{
+  public:
+
+    vector<vector<int> > levelOrderBottom(TreeNode *root){
+        std::queue<TreeNode*> q;
+        std::queue<TreeNode*> tmp;
+        vector<vector<int> > ans;
+        if (root){
+            q.push(root);
+        }
+        std::vector<TreeNode*> last;
+        while (!q.empty()){
+            std::vector<int> tvec;
+            while (!q.empty()){
+                tmp.push(q.front());     
+                tvec.push_back(q.front()->val); 
+                q.pop();
+            }
+            ans.push_back(std::move(tvec));
+            while (!tmp.empty()){
+                auto now = tmp.front();
+                if (now->left){
+                    q.push(now->left);
+                }
+                if (now->right){
+                    q.push(now->right);
+                }
+                tmp.pop();
+            }
+        }
+
+        for (int i = 0, j = ans.size() - 1; i < j; ++i, --j){
+            std::swap(ans[i], ans[j]);
+
+        }
+
+        return ans;
+    }
+};
+
+
+
+int main(){
+    Solution slu;
+
+    auto root = Serialize::treenode({3,9,20,inf,inf,15,7});
+
+    auto ans = slu.levelOrderBottom(root);
+
+    for (auto &a : ans){
+        for (auto &t : a){
+            cout << t << " ";
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
+
